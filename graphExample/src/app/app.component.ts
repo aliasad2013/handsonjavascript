47cd8bcae5caf85ec3b7acd95b3854b13cef6526
ali <ali.asad@live.com>
2019-01-07 00:08:13 -0600

    committed

--- graphExample/src/app/app.component.ts
+++ graphExample/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GraphService } from './utils/services/graph.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'graphExample';
+  constructor() {
+    let g = new GraphService(6);
+    g.addEdge(1, 2);
+    g.addEdge(2, 5);
+    g.addEdge(1, 3);
+    g.addEdge(1, 4);
+    g.addEdge(0, 1);
+
+    g.vertexList = ['CS1', 'CS2', 'Data Structures',
+      'Assembly Language', 'Operating Systems',
+      'Algorithms'];
+    g.showGraph();
+    g.topSort();
+    // let vertex = 4;
+    // let paths = g.shortestPathTo(vertex);
+    // console.log(paths);
+    // while (paths.length > 0) {
+    //   if (paths.length > 1) {
+    //     console.log(paths.pop() + '-');
+    //   }
+    //   else {
+    //     console.log(paths.pop());
+    //   }
+    // }
+  }
 }
