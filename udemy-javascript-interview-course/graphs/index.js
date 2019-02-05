5023c9446cb3f69667e211d6768f408b51891de1
ali <ali.asad@live.com>
2019-02-04 21:53:07 -0600

    dfs recursive v iterative

--- udemy-javascript-interview-course/graphs/index.js
+++ udemy-javascript-interview-course/graphs/index.js
@@ -1,15 +1,70 @@
 class graph {
     constructor(numberOfNodes) {
-        this.adjList = new Array(numberOfNodes).fill([]);
         this.length = numberOfNodes;
+        this.adjList = new Array(this.length);
+        this.visited = {};
+        this.visitedSequence = [];
         console.log(this.adjList);
     }
 
     addEdges(node1, node2) {
-        this.adjList[node1].push(node2)
+        if (!this.adjList[node1]) this.adjList[node1] = [];
+        this.adjList[node1].push(node2);
+    }
+    showGraph() {
+        return this.adjList;
+    }
+    //Following is the Recursive method to perform Depth First Search
+    dfs_traverse(node) {
+        if (!this.adjList[node]) return;
+        this.visited[node] = true;
+        if (this.visitedSequence.length == 0) this.visitedSequence.push(node);
+        this.adjList[node].forEach((value) => {
+            if (this.visited[value]) return;
+            this.visited[value] = true;
+            this.visitedSequence.push(value);
+            this.dfs_traverse(value);
+        });
+        return this.visitedSequence;
     }
 
-    dfs_traverse() { }
+    //Following is the Iterative method to perform Depth First Search in a Graph
+    dfs_traverse_Iterative_version(node) {
+        if (!this.adjList[node]) return;
+        let list = [node];
+
+        while (list.length > 0) {
+            node = list.shift();
+            if (!this.visited[node]) {
+                console.log(node, list);
+                this.visited[node] = true;
+                this.visitedSequence.push(node);
+                if (this.adjList[node]) {
+                    list.unshift(...this.adjList[node]);
+                }
+            }
+        }
+        return this.visitedSequence;
+    }
+
+
+    bfs_traverse(node) {
+        if (!this.adjList[node]) return;
+        let list = [node];
+
+        while (list.length > 0) {
+            node = list.shift();
+            if (!this.visited[node]) {
+                console.log(node, list);
+                this.visited[node] = true;
+                this.visitedSequence.push(node);
+                if (this.adjList[node]) {
+                    list.unshift(...this.adjList[node]);
+                }
+            }
+        }
+        return this.visitedSequence;
+    }
 }
 
 
--- udemy-javascript-interview-course/graphs/test.js
+++ udemy-javascript-interview-course/graphs/test.js
@@ -9,24 +9,51 @@ test('graph class exists', () => {
     expect(graph.length).toEqual(5);
 });
 
-test('graph class exists', () => {
+test('graph traverse DFS', () => {
     let graph = new Graph(5);
     graph.addEdges(0, 1);
     graph.addEdges(0, 2);
-
     graph.addEdges(1, 0);
     graph.addEdges(1, 3);
-    //graph.addEdges(1, 4);
-
     graph.addEdges(2, 0);
     graph.addEdges(2, 4);
-
     graph.addEdges(3, 1);
     graph.addEdges(3, 4);
-
-    graph.addEdges(4, 2);
     graph.addEdges(4, 5);
-
-    expect(graph.length).toEquals(5);
+    expect(graph.dfs_traverse(0)).toEqual([0, 1, 3, 4, 5, 2]);
 });
 
+test('Testing graph traverse using DFS', () => {
+    let graph = new Graph(5);
+    graph.addEdges(1, 0);
+    graph.addEdges(0, 3);
+    graph.addEdges(0, 2);
+    graph.addEdges(2, 1);
+    graph.addEdges(1, 4);
+    expect(graph.dfs_traverse(0)).toEqual([0, 3, 2, 1, 4]);
+});
+
+test('Testing graph traversal using DFS Iterative version', () => {
+    let graph = new Graph(5);
+    graph.addEdges(1, 0);
+    graph.addEdges(0, 3);
+    graph.addEdges(0, 2);
+    graph.addEdges(2, 1);
+    graph.addEdges(1, 4);
+    expect(graph.dfs_traverse_Iterative_version(0)).toEqual([0, 3, 2, 1, 4]);
+});
+
+
+test('Testing graph traversal using DFS Iterative version', () => {
+    let graph = new Graph(5);
+    graph.addEdges(0, 1);
+    graph.addEdges(0, 2);
+    graph.addEdges(1, 0);
+    graph.addEdges(1, 3);
+    graph.addEdges(2, 0);
+    graph.addEdges(2, 4);
+    graph.addEdges(3, 1);
+    graph.addEdges(3, 4);
+    graph.addEdges(4, 5);
+    expect(graph.dfs_traverse_Iterative_version(0)).toEqual([0, 1, 3, 4, 5, 2]);
+});
